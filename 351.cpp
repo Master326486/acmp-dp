2d8c29ba3a0eb8f5c20d20cd16cda1539812e0ed
TMQuitten <master32wsp@gmail.com>
2025-08-03 21:18:33 +0300

    351

---
+++ 351.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+using i128 = __int128;
+using u128 = __int128;
+#define all(x) x.begin(), x.end()
+
+#define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
+#define FOR2(i, a) for (ll i = 0; i < ll(a); ++i)
+#define FOR3(i, a, b) for (ll i = a; i < ll(b); ++i)
+#define FOR4(i, a, b, c) for (ll i = a; i < ll(b); i += (c))
+#define FOR1_R(a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR2_R(i, a) for (ll i = (a)-1; i >= ll(0); --i)
+#define FOR3_R(i, a, b) for (ll i = (b)-1; i >= ll(a); --i)
+#define overload4(a, b, c, d, e, ...) e
+#define overload3(a, b, c, d, ...) d
+#define FOR(...) overload4(__VA_ARGS__, FOR4, FOR3, FOR2, FOR1)(__VA_ARGS__)
+#define FOR_R(...) overload3(__VA_ARGS__, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
+
+const int infty = 1'000'000'000;
+void solve() {
+    int N, K; cin >> N >> K;
+    string S; cin >> S;
+    vector<int> A(N); FOR(i, N) A[i] = S[i] - 'A';
+    vector<int> x(26, -infty); x[A[0]] = 0;
+    vector<int> dp(N, infty); dp[0] = 0;
+    FOR(i, 1, N) {
+        FOR(j, 26) if (i - x[j] <= K)
+            dp[i] = min(dp[i], dp[x[j]] + (A[i] != j));
+        x[A[i]] = i;
+    }
+    cout << dp[N - 1] << endl;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("input.txt", "r+", stdin);
+    clock_t start_time = clock();
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    cout.flush();
+
+#ifdef LOCAL
+    fprintf(stderr, "\n>> Exec Time: %.3f ms\n", (double)(clock() - start_time) / CLOCKS_PER_SEC * 1000);
+#endif
+    return 0;
+}
